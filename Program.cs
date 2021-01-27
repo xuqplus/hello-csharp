96818011639757a9b1c68200c3fb36634d2e31ec
Qunqun Xu <qunqun.xu@autodesk.com>
2021-01-27 16:58:49 +0800

    Re-initialize c sharp project

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 
-namespace hello_csharp
+namespace HelloCSharp
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadKey();
         }
     }
 }
