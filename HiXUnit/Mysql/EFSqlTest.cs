586af9c95b74d837f17e37463464bb5759e1fcd9
Qunqun Xu <qunqun.xu@autodesk.com>
2021-02-07 17:55:03 +0800

    temp

---
+++ HiXUnit/Mysql/EFSqlTest.cs
@@ -0,0 +1,36 @@
+﻿using MySql.Data.MySqlClient;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+using System.Runtime.InteropServices;
+
+namespace HiXUnit.Mysql
+{
+    public class EFSqlTest
+    {
+        private ITestOutputHelper OutputHelper { get; }
+
+        public EFSqlTest(ITestOutputHelper outputHelper)
+        {
+            OutputHelper = outputHelper;
+        }
+
+        [Fact]
+        public void Test()
+        {
+            OutputHelper.WriteLine("this is xunit - MysqlTest");
+
+            string name = "xqq";
+
+            using (var context = new UserContext())
+            {
+                context.Users.FromSqlRaw("select");
+                OutputHelper.WriteLine("idddd=>{2}, ");
+                Assert.True(0 != 1);
+            }
+        }
+    }
+}
